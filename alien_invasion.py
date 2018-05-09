07187e8daf52e78e366ce8cbafe72de5b214280c
Shamari Southwell <shamari25southwell@gmail.com>
2018-05-08 21:26:23 -0400

    initial commit

---
+++ alien_invasion.py
@@ -0,0 +1,24 @@
+import sys
+
+import pygame
+
+from settings import Settings
+
+
+def run_game():
+    # Initailize game and create a screen object
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screenwidth, ai_settings.screenheight))
+    pygame.display.set_caption("Alien Invasion")
+
+    while True:
+        screen.fill(ai_settings.bg_color)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        pygame.display.flip()
+
+
+run_game()
---
+++ settings.py
@@ -0,0 +1,6 @@
+class Settings():
+
+    def __init__(self):
+        self.screenwidth = 1200
+        self.screenheight = 800
+        self.bg_color = (0, 0, 0)
